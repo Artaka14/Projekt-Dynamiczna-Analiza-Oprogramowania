a52ca0990d00b2550476d634981cbdde01246a2b
Artaka14 <103130398+Artaka14@users.noreply.github.com>
2025-10-23 15:29:02 +0200

    Update and rename ProjektDAO.py to main.py

--- main.py
+++ main.py
@@ -1,85 +1,9 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import customtkinter
 import time
 import threading
 from PIL import Image
-import pandas as pd
-
-#Funkcja, która zdobywa informacje o kursie CDPu do wykresu
-def getCdpData(period): 
-    end_time = datetime.now()
-
-    if end_time.weekday() >= 5: 
-       days_to_subtract = end_time.weekday() - 4 
-       end_time -= timedelta(days=days_to_subtract)
-
-    if period == "1d":
-        start_time = end_time - timedelta(days=1)
-        interval = "1m"
-    elif period == "7d":
-        start_time = end_time - timedelta(days=7)
-        interval = "1m"
-    elif period == "1m":
-        start_time = end_time - timedelta(days=30)
-        interval = "15m"
-
-    data = yf.download("CDR.WA", start=start_time, end=end_time, interval=interval, progress=False)
-
-    if data.empty:
-        raise ValueError("Brak danych dla wybranego okresu.")
-
-    data_reset = data.reset_index(drop=False)
-    data_reset["sample_index"] = range(len(data_reset))
-
-    return data_reset
-
-#Funkcja tworząca wykres
-def createCdpPlot(frame, period):
-    data_reset = getCdpData(period)
-
-    if period == "1d":
-        periodName = "1 dzień"
-    elif period == "7d":
-        periodName = "7 dni"
-    elif period == "1m":
-        periodName = "1 miesiąc"
-        period = "1mo"
-
-    fig, ax = plt.subplots(figsize=(8, 4))
-
-    ax.plot(data_reset["sample_index"], data_reset["Close"], label="CD Projekt S.A.")
-    ax.set_title(f"CD Projekt S.A. — okres: {periodName}")
-    ax.set_ylabel("Cena (PLN)")
-
-    session_bounds = data_reset.groupby(data_reset["Datetime"].dt.date)["sample_index"].agg(['min', 'max'])
-
-    ticks = session_bounds['min'].tolist()
-    labels = [str(d) for d in session_bounds.index]
-    ticks.append(session_bounds['max'].iloc[-1])
-    labels.append(str(session_bounds.index[-1]+timedelta(days=1)))
-
-    ax.set_xticks(ticks)
-    ax.set_xticklabels(labels, rotation=45)
-
-    ax.set_xlim(data_reset['sample_index'].min(), data_reset['sample_index'].max())
-
-    ax.grid(True)
-    plt.tight_layout()
-
-    canvas = FigureCanvasTkAgg(fig, master=frame)
-    canvas.draw()
-    canvas.get_tk_widget().pack(fill="both", expand=True)
-
-#Funkcja, która pobiera informacje o kursie
-def getCurrentPrice():
-    ticker = yf.Ticker("CDR.WA")
-    data = ticker.history(period="1d", interval="1m")
-    if not data.empty:
-        return round(data["Close"].iloc[-1], 2)
-    return None
+import CDPplot
+import CDPprice
 
 class SplashScreen(customtkinter.CTk):
     def __init__(self):
@@ -109,7 +33,7 @@ class SplashScreen(customtkinter.CTk):
     def load_data_with_delay(self):
         start_time = time.time()
         try:
-            data = getCdpData("7d")
+            data = CDPplot.getCdpData("7d")
         except Exception as e:
             print("Błąd pobierania danych:", e)
             data = None
@@ -172,20 +96,21 @@ class App(customtkinter.CTk):
             
          # Jak sa dane pobrane w splash to uzywane
         if preloaded_data is not None:
-             createCdpPlot(self.plot_frame, "7d")  
+             CDPplot.createCdpPlot(self.plot_frame, "7d")  
         self.updatePriceLabel()
 
     def showPlot(self, period):
         for widget in self.plot_frame.winfo_children():
             widget.destroy()
-        createCdpPlot(self.plot_frame, period)
+        CDPplot.createCdpPlot(self.plot_frame, period)
         self.state("zoomed")
 
     def updatePriceLabel(self):
-        price = getCurrentPrice()
+        price = CDPprice.getCurrentPrice()
         self.price_label_value.configure(text=f"{price} PLN")
         self.state("zoomed")
 
-start = SplashScreen()
-start.mainloop()
+if __name__ == "__main__":
+   start = SplashScreen()
+   start.mainloop()
 
