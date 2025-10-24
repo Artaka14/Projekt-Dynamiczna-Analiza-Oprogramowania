bee065dfc5220e7c1555fc744a44f86624f9105e
Daro <darekolodz5@gmail.com>
2025-10-24 23:59:37 +0200

    Refactor main.py to include SplashScreen
    Refactor main.py to integrate SplashScreen class and improve application structure.

--- main.py
+++ main.py
@@ -4,50 +4,7 @@ import threading
 from PIL import Image
 import CDPplot
 import CDPprice
-
-class SplashScreen(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        self.title("Ekran startowy")
-
-        width, height = 400, 250
-        self.geometry(f"{width}x{height}+{self.center_x(width)}+{self.center_y(height)}")
-        self.resizable(False, False)
-        self.overrideredirect(True)
-
-        # XDlogo
-        self.logo = customtkinter.CTkImage(light_image=Image.open("asety\XDlogo.png"), size=(400, 250))
-        customtkinter.CTkLabel(self, image=self.logo, text="").pack()
-
-        # pobierańsko w tle
-        threading.Thread(target=self.load_data_with_delay, daemon=True).start()
-
-    def center_x(self, window_width):
-        screen_width = self.winfo_screenwidth()
-        return int((screen_width / 2) - (window_width / 2))
-
-    def center_y(self, window_height):
-        screen_height = self.winfo_screenheight()
-        return int((screen_height / 2) - (window_height / 2))
-
-    def load_data_with_delay(self):
-        start_time = time.time()
-        try:
-            data = CDPplot.getCdpData("7d")
-        except Exception as e:
-            print("Błąd pobierania danych:", e)
-            data = None
-
-        elapsed = time.time() - start_time
-        if elapsed < 2.5:
-            time.sleep(2.5 - elapsed)
-
-        self.after(0, lambda: self.open_main_app(data))
-
-    def open_main_app(self, data):
-        self.destroy()
-        app = App(preloaded_data=data)
-        app.mainloop()
+import Splash
 
 class App(customtkinter.CTk):
     def __init__(self, preloaded_data=None):
@@ -113,4 +70,5 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
    start = SplashScreen()
    start.mainloop()
+
 
