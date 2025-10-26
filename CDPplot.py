88a5a5026096b255959530cf9e73aeb8c998f053
Artaka14 <103130398+Artaka14@users.noreply.github.com>
2025-10-26 10:57:26 +0100

    Update CDPplot.py

--- CDPplot.py
+++ CDPplot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import yfinance as yf
+import CDPdata
 import main
 from datetime import datetime, timedelta
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from CTkMessagebox import CTkMessagebox
 
 #Funkcja, która zdobywa informacje o kursie CDPu do wykresu
 def getCdpData(period): 
@@ -71,3 +73,47 @@ def createCdpPlot(frame, period, data):
     canvas.draw()
 
     canvas.get_tk_widget().pack(fill="both", expand=True)
+
+def createCustomDataCdpPlot(frame, start_date, end_date):
+        today = datetime.now().date()
+
+        if start_date > end_date:
+            CTkMessagebox(
+                title="Błędny zakres dat",
+                message="Data początkowa nie może być późniejsza niż końcowa.",
+                icon="cancel"
+            )
+            return
+
+        if start_date > today or end_date > today:
+            CTkMessagebox(
+                title="Błędny zakres dat",
+                message="Nie można wybrać dat z przyszłości.",
+                icon="warning"
+            )
+            return
+
+        data = CDPdata.getCustomCdpData(start_date, end_date)
+        if data.empty:
+           raise ValueError("Brak danych dla wybranego zakresu.")
+
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.plot(data.index, data["Close"], label="CD Projekt S.A.")
+        ax.set_title(f"CD Projekt S.A. — {start_date} → {end_date}")
+        ax.set_ylabel("Cena (PLN)")
+
+        session_bounds = data.groupby(data["Datetime"].dt.date)["sample_index"].agg(['min', 'max'])
+        ticks = session_bounds['min'].tolist()
+        labels = [str(d) for d in session_bounds.index]
+        ticks.append(session_bounds['max'].iloc[-1])
+        labels.append(str(session_bounds.index[-1]+timedelta(days=1)))
+
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(labels, rotation=45)
+
+        ax.grid(True)
+        plt.tight_layout()
+
+        canvas = FigureCanvasTkAgg(fig, master=frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill="both", expand=True)
